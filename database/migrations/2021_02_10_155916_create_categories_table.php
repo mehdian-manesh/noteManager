1014674c66a59228c3d56e601756be2e9a8c0f78
hassan <mehdian92@gmail.com>
2021-02-15 01:24:00 +0330

    initial commit

---
+++ database/migrations/2021_02_10_155916_create_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',100)->index();
+            $table->foreignId('user_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // $table->dropForeign('categories_user_id_foreign');
+        Schema::dropIfExists('categories');
+    }
+}
